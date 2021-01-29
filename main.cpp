bd0b149354e2f135c2665f125da965ced1e2960a
Kip <kiptrujillo@protonmail.com>
2021-01-29 15:05:51 -0800

    added getConfigFileList() - to hunt for config files dynamically
    The target is to spawn a new thread for every config file present - so i
    don't have to hardcode spawn nodes. 1 config file = 1 NODE thread

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 // Main
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <filesystem>
+#include <regex>
 
 using namespace std;
 
@@ -10,6 +13,9 @@ class Node {
 	int writeport = 1;
 	
 	int start(string filename);
+	void test();
+	Node();
+	Node(string filename);
 	
 
 };
@@ -41,26 +47,68 @@ int Node::start(string filename){
 	return 0;
 }
 
-int main(){
+Node::Node(string filename){
+	//std::thread t(&Node::test, this);
+	
+	
+}
+void Node::test(){
+	cout<<"tatas"<<endl;
+}
 
+Node::Node(){
+
+}
+
+string getConfigFileList(){
 /*
+string files[3] = {"confA.txt", "confB.txt", "confC.txt"};
+*/
+	regex r ("./conf[A-Z].txt");
+	std::string path = "./";
+	string result = "";
+
+	bool firstLoop = true;
+	for (const auto & entry : std::filesystem::directory_iterator(path)){
+		//Debug line
+		std::cout << "Found File: " << entry.path() << std::endl;
+		
+		string l = entry.path(); 
+		if(regex_match(l,r)){
+			if(firstLoop == true){
+			//Do cool string concat/append 
+				result = l;
+				firstLoop = false;
+			} else {
+				result = result + "," + l;
+			}
+		}
+	}
+	return result;
+}
+
+/************************************************
 nodeA : listens @ NULL, sends to B
 nodeB : listens @ 5000, sends to C
 nodeC : listens @ 5005, sends to NULL
 
 Each node receives their port congigurations from it's configuration file. The file should be located in the root of the startup directory. 
+*************************************************/
+int main(){
+	cout << getConfigFileList() <<endl;
 
-
-*/
-	cout << "hello world\n";
 	Node nodeA;
 	nodeA.listenport = 8080;
 	cout << nodeA.listenport << endl;
-	nodeA.start("yeet.txt");
+	nodeA.start("./yeet.txt");
 	
-	Node nodeB;
-	nodeB.start("foo.xyz");
-	
+	Node nodeB("./yeet.txt");
+
+	//for(int i = 0; i<){}
+
+	std::thread th1(&Node::test, Node());
+	//std::thread th2()
+	th1.join();
 	
 	return 0;
 }
